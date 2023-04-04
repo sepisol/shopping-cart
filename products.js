75afe381af82d30d8e803da67be12414d1bc1081
sepi <spdsoli@gmail.com>
2023-04-04 22:52:53 +0430

    create code

---
+++ products.js
@@ -0,0 +1,51 @@
+export const productsData = [
+  
+  {
+    id: 1,
+    title: "Chanel",
+    price:250,
+    imageUrl: "./images/dior5.jpg",
+  },
+  {
+    id: 2,
+    title: "Ck",
+    price: 450,
+    imageUrl: "./images/ck2.jpg",
+  },
+  {
+    id: 3,
+    title:"Versace",
+    price: 350,
+    imageUrl: "./images/versace5.jpg",
+  },
+  {
+    id: 4,
+    title: "Dior",
+    price:200,
+    imageUrl: "./images/dior5.jpg",
+  },
+  {
+    id: 5,
+    title:"Versace",
+    price: 350,
+    imageUrl: "./images/versace5.jpg",
+  },
+  {
+    id: 6,
+    title: "Dior",
+    price:200,
+    imageUrl: "./images/dior5.jpg",
+  },
+  {
+    id: 7,
+    title:"Versace",
+    price: 350,
+    imageUrl: "./images/versace5.jpg",
+  },
+  {
+    id: 8,
+    title: "Dior",
+    price:200,
+    imageUrl: "./images/dior5.jpg",
+  },
+];
